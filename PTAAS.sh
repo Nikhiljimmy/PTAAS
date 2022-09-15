e7fa785e29fe5e105f3c1e6be7f2a80d3878be76
kishangupta94 <kishangupta9494@gmail.com>
2022-09-15 20:56:51 +0530

    ptaas service5

--- PTAAS.sh
+++ PTAAS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash launch_burp.sh http assets.winni.com 80 / mohinikumbhar44@gmail.com --user-config-file=user.json
+bash launch_burp.sh http assets.winni.com 80 / kishangupta9494@gmail.com --user-config-file=user.json
 
