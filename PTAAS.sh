a59221348d30393e5fe9feb97440e518a939d08e
kishangupta94 <kishangupta9494@gmail.com>
2022-09-15 22:24:50 +0530

    final mutt setup

--- PTAAS.sh
+++ PTAAS.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash launch_burp.sh http assets.winni.com 80 / soropa8925@iunicus.com --user-config-file=user.json
+bash launch_burp.sh http assets.winni.com 80 / kishangupta9494@gmail.com --user-config-file=user.json
