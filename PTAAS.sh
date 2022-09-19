a1b6b8a6fd6fbf324a84773d804f8fba507d3e7a
Nikhiljimmy <patiljimmy22@gmail.com>
2022-09-19 01:41:30 -0700

    PTAAS8

--- PTAAS.sh
+++ PTAAS.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash launch_burp.sh http testphp.vulweb.com 80 / kishanmadheshiya9494@gmail.com --user-config-file=user.json
+bash launch_burp.sh http assets.winni.in 80 / kishanmadheshiya9494@gmail.com --user-config-file=user.json
